e10eb0e5c86f38a320acb90d8d63538a599664b0
Sabbir <webexpertsabbir@gmail.com>
2022-08-09 12:12:42 +0600

    unique

---
+++ duplicate.js
@@ -0,0 +1,14 @@
+const names = ['Sabbir', 'Rasel', 'Nasair', 'Sabbir', 'Rasel', 'Nasair',];
+
+function uniqueName(names){
+    const unique = [];
+    for(let i = 0; i < names.length; i++){
+        const name = names[i];
+        if(unique.includes(name) == false){
+            unique.push(name);
+        }
+    }
+    return unique;
+}
+const finalName = uniqueName(names);
+console.log(finalName);
